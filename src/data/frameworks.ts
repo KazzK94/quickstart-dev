f51fa2bda4193e6566caefecbad942a1605d6f7b
Kazz <i94773007@gmail.com>
2024-11-04 16:04:24 +0100

    added astro to quickstart guides

--- src/data/frameworks.ts
+++ src/data/frameworks.ts
@@ -16,21 +16,21 @@ export const frameworks = [
 				command: "npm install"
 			},
 			{
-				label: "Install Tailwind CSS",
+				label: "(optional) Install Tailwind CSS",
 				command: "npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p"
 			},
 			{
-				label: "Configure Tailwind CSS (steps 3 and 4 on Tailwind CSS documentation for usage on Vite):",
+				label: "(optional) Configure Tailwind CSS (steps 3 and 4 on Tailwind CSS documentation for usage on Vite):",
 				command: "https://tailwindcss.com/docs/guides/vite",
 				isUrl: true
 			},
 			{
-				label: 'Before installing shadcn (steps 3, 4 and 5 on shadcn documentation for usage on Vite):',
+				label: '(optional) Before installing shadcn (steps 3, 4 and 5 on shadcn documentation for usage on Vite):',
 				command: 'https://ui.shadcn.com/docs/installation/vite',
 				isUrl: true
 			},
 			{
-				label: "Install shadcn/ui",
+				label: "(optional) Install shadcn/ui",
 				command: "npx shadcn-ui@latest init"
 			},
 			{
@@ -51,8 +51,29 @@ export const frameworks = [
 				command: "cd [app-name]"
 			},
 			{
-				label: "Install shadcn/ui",
+				label: "(optional) Install shadcn/ui",
 				command: "npx shadcn-ui@latest init"
+			},
+			{
+				label: "Start Development Server",
+				command: "npm run dev"
+			}
+		]
+	},
+	{
+		name: "Astro",
+		steps: [
+			{
+				label: "Initialize Project",
+				command: "npm create astro@latest"
+			},
+			{
+				label: "Navigate to Project",
+				command: "cd [app-name]"
+			},
+			{
+				label: "Add Tailwind to your project (gets configured automatically)",
+				command: "npx astro add tailwind"
 			},
 			{
 				label: "Start Development Server",
